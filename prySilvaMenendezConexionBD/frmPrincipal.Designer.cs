eb29130e7e731cd302a2a51b52dbea4c358996cf
JoacoSM18 <joako.sm.18@gmail.com>
2026-03-26 09:33:54 -0300

    Estado de Conexion

--- prySilvaMenendezConexionBD/frmPrincipal.Designer.cs
+++ prySilvaMenendezConexionBD/frmPrincipal.Designer.cs
@@ -28,22 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblEstadoConexion = new System.Windows.Forms.StatusStrip();
+            this.lblEstadoConexion1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblEstadoConexion.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblEstadoConexion
+            // 
+            this.lblEstadoConexion.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblEstadoConexion1});
+            this.lblEstadoConexion.Location = new System.Drawing.Point(0, 339);
+            this.lblEstadoConexion.Name = "lblEstadoConexion";
+            this.lblEstadoConexion.Size = new System.Drawing.Size(384, 22);
+            this.lblEstadoConexion.TabIndex = 0;
+            this.lblEstadoConexion.Text = "statusStrip1";
+            // 
+            // lblEstadoConexion1
+            // 
+            this.lblEstadoConexion1.Name = "lblEstadoConexion1";
+            this.lblEstadoConexion1.Size = new System.Drawing.Size(118, 17);
+            this.lblEstadoConexion1.Text = "toolStripStatusLabel1";
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.lblEstadoConexion);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
             this.Name = "frmPrincipal";
             this.Text = "Conexion Base de Datos en Access";
             this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.lblEstadoConexion.ResumeLayout(false);
+            this.lblEstadoConexion.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip lblEstadoConexion;
+        private System.Windows.Forms.ToolStripStatusLabel lblEstadoConexion1;
     }
 }
 
--- prySilvaMenendezConexionBD/frmPrincipal.cs
+++ prySilvaMenendezConexionBD/frmPrincipal.cs
@@ -20,7 +20,23 @@ namespace prySilvaMenendezConexionBD
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
             ClassConexionBD objConectarBD = new ClassConexionBD();
-            objConectarBD.ConectarBD();
+
+            try
+            {
+                objConectarBD.ConectarBD();
+                lblEstadoConexion1.Text = "Base Conectada";
+                lblEstadoConexion1.BackColor = Color.Green;
+            }
+            catch (Exception)
+            {
+                lblEstadoConexion.Text = "Sin Conexión";
+                lblEstadoConexion.BackColor = Color.Red;
+            }
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
